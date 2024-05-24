cc556d80df3ee860e16b1e20530cac5664e7d980
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-05-24 12:21:11 +0300

    finished hw-03

--- src/js/hw-03/cats-api.js
+++ src/js/hw-03/cats-api.js
@@ -1,5 +1,22 @@
-import { BASE_URL, API_KEY } from './config';
+import { BASE_URL, API_KEY } from './config.js';
+import { refs } from './index.js';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const toastOptions = {
+  title: '❌ ',
+  message: `Oops! Something went wrong! Try reloading the page!`,
+  backgroundColor: 'tomato',
+  icon: '',
+  messageColor: 'white',
+  position: 'center',
+  timeout: 900,
+  close: false,
+  animateInside: false,
+  progressBar: false,
+  transitionIn: 'bounceInUp',
+};
 const requestOptions = {
   method: 'GET',
   headers: {
@@ -8,12 +25,32 @@ const requestOptions = {
   },
 };
 
+const showErrorMsg = () => {
+  refs.errorMsg.innerHTML = `<div>
+    <div class="frame">
+      <iframe
+        src="https://giphy.com/embed/jpctIaLRTNCKKGiyPd/video"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        allowfullscreen=""
+      ></iframe>
+    </div>
+    <p>Oops...</p>
+  </div>`;
+};
+
 export const fetchBreeds = async () => {
   try {
     const response = await fetch(`${BASE_URL}/breeds`, requestOptions);
+    if (!response.ok) {
+      throw new Error();
+    }
     return response.json();
   } catch (error) {
-    throw new Error(error);
+    iziToast.error(toastOptions);
+    setTimeout(showErrorMsg, 1000);
+    console.log(error);
   }
 };
 
@@ -23,8 +60,13 @@ export const fetchCatByBreed = async breedId => {
       `${BASE_URL}/images/search?breed_ids=${breedId}`,
       requestOptions
     );
+    if (!response.ok) {
+      throw new Error();
+    }
     return response.json();
   } catch (error) {
-    throw new Error(error);
+    iziToast.error(toastOptions);
+    setTimeout(showErrorMsg, 1000);
+    console.log(error);
   }
 };
--- src/js/hw-03/index.js
+++ src/js/hw-03/index.js
@@ -3,30 +3,61 @@ import SlimSelect from 'slim-select';
 
 import 'slim-select/styles';
 
-const refs = {
+export const refs = {
   select: document.querySelector('select.breed-select'),
   cardWrapper: document.querySelector('.cat-info'),
   backdrop: document.querySelector('.backdrop'),
+  errorMsg: document.querySelector('.error-msg'),
 };
+
 const createOptionsMarkup = data => {
   return data.reduce((markup, { name, id }) => {
     return (markup += `<option value="${id}">${name}</option>`);
-  }, '');
+  }, '<option data-placeholder="true"></option>');
+};
+const createCatMarkup = ({ breeds, url }) => {
+  const [params] = breeds;
+  return `<article class="cat-card">
+    <div class="cat-card-left">
+      <img
+        class="cat-card-img"
+        src="${url}"
+        alt="${params.name}"
+      />
+    </div>
+    <div class="cat-card-right">
+      <h2 class="cat-card-title">${params.name}</h2>
+      <p class="cat-card-desc">${params.description}</p>
+      <p class="cat-card-tepm">
+        <strong>Temperament:</strong>
+        ${params.temperament}
+      </p>
+    </div>
+  </article>`;
 };
 const onSelectChange = async event => {
   const selectedOptionValue = event.currentTarget.value;
+  refs.backdrop.classList.toggle('is-hidden');
   const response = await fetchCatByBreed(selectedOptionValue);
   const catInfo = response[0];
-  console.log('onSelectChange  response:', catInfo);
+  refs.cardWrapper.innerHTML = createCatMarkup(catInfo);
+  refs.backdrop.classList.toggle('is-hidden');
 };
-
 const initBreeds = async () => {
   const breeds = await fetchBreeds();
+  if (!breeds) {
+    refs.select.style.display = 'none';
+    refs.backdrop.classList.toggle('is-hidden');
+    return;
+  }
+
   refs.backdrop.classList.toggle('is-hidden');
-  const optionsMarkup = await createOptionsMarkup(breeds);
-  refs.select.innerHTML = optionsMarkup;
+  refs.select.innerHTML = await createOptionsMarkup(breeds);
   new SlimSelect({
     select: 'select.breed-select',
+    settings: {
+      placeholderText: 'Select the cat breed',
+    },
   });
 };
 
