37487377459884ffa4ce51d863c7a619f76f7d85
YK911 <55898053+YK911@users.noreply.github.com>
2024-05-29 14:42:53 +0000

    Deploying to gh-pages from @ neoversity-woolf/goit-advancedjs@cddc556d7fdd8a3e10790340eab5569d39c556bc 🚀

--- commonHelpers11.js
+++ commonHelpers11.js
@@ -1,20 +1,20 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as E,b,S as C,c as x}from"./assets/vendor-ec4be6ce.js";const M="https://pixabay.com/api/",k="15249615-5ccf49bef51d4f01888f64cb2",f=40,d=({message:t,customClass:e})=>{const o=document.getElementById("toastContainer"),s=document.createElement("div");s.classList.add("toast","align-items-center",e),s.setAttribute("role","alert"),s.setAttribute("aria-live","assertive"),s.setAttribute("aria-atomic","true"),s.innerHTML=`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as $,b as f,S as C,c as x}from"./assets/vendor-ec4be6ce.js";const M="https://pixabay.com/api/",k="15249615-5ccf49bef51d4f01888f64cb2",p=40,d=({message:t,customClass:e})=>{const o=document.getElementById("toastContainer"),s=document.createElement("div");s.classList.add("toast","align-items-center",e),s.setAttribute("role","alert"),s.setAttribute("aria-live","assertive"),s.setAttribute("aria-atomic","true"),s.innerHTML=`
       <div class="d-flex">
         <div class="toast-body text-white">
             ${t}
         </div>
         <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
       </div>
-  `,o.appendChild(s),new E(s,{delay:2e3}).show()};b.defaults.baseURL=M;const q={key:k,per_page:f,safesearch:!0,image_type:"photo",orientation:"horizontal"},p=async({searchQuery:t,currentPage:e}={})=>{const o=await b({params:{...q,q:t,page:e}}),{data:s,status:i}=o,a=Math.ceil(s.total/f),r=e>=a||a===1;return i!==200?(d({message:"Sorry, something went wrong on server.",customClass:"bg-danger"}),{hits:s.hits,isEndOfCollection:r}):s.total?(e<2&&d({message:`Hooray! We found ${s.totalHits} images.`,customClass:"bg-success"}),r?(d({message:"End of collection.",customClass:"bg-warning"}),{hits:s.hits,isEndOfCollection:r}):{hits:s.hits,isEndOfCollection:r}):(d({message:"Sorry, there are no images matching your search query. Please try again.",customClass:"bg-info"}),{hits:s.hits,isEndOfCollection:r})},v=t=>t.reduce((e,o)=>{const{webformatURL:s,webformatWidth:i,webformatHeight:a,largeImageURL:r,tags:u,likes:l,views:S,comments:T,downloads:A}=o;return e+=`<a
+  `,o.appendChild(s),new $(s,{delay:2e3}).show()};f.defaults.baseURL=M;const q={key:k,per_page:p,safesearch:!0,image_type:"photo",orientation:"horizontal"},v=async({searchQuery:t,currentPage:e}={})=>{const o=await f({params:{...q,q:t,page:e}}),{data:s,status:c}=o,a=Math.ceil(s.total/p),n=e>=a||a===1;return c!==200?(d({message:"Sorry, something went wrong on server.",customClass:"bg-danger"}),{hits:s.hits,isEndOfCollection:n}):s.total?(e<2&&d({message:`Hooray! We found ${s.totalHits} images.`,customClass:"bg-success"}),n?(d({message:"End of collection.",customClass:"bg-warning"}),{hits:s.hits,isEndOfCollection:n}):{hits:s.hits,isEndOfCollection:n}):(d({message:"Sorry, there are no images matching your search query. Please try again.",customClass:"bg-info"}),{hits:s.hits,isEndOfCollection:n})},u="/goit-advancedjs/sprite.svg",y=t=>t.reduce((e,o)=>{const{webformatURL:s,webformatWidth:c,webformatHeight:a,largeImageURL:n,tags:m,likes:l,views:T,comments:A,downloads:E}=o;return e+=`<a
           class="info-card d-flex flex-column text-decoration-none"
-          href="${r}"
+          href="${n}"
         >
           <div class="rounded overflow-hidden flex-grow-1 shadow">
             <img
               class="info-ill"
               src="${s}"
-              alt="${u}"
-              width="${i}"
+              alt="${m}"
+              width="${c}"
               heigth="${a}"
             />
           </div>
@@ -28,7 +28,7 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as E,b,S as C,c as 
               data-bs-title="Likes: ${l}"
             >
               <svg class="info-icon" aria-hidden="true" width="20" height="20">
-                <use href="/sprite.svg#likes"></use>
+                <use href="${u}#likes"></use>
               </svg>
             </button>
             <button
@@ -37,10 +37,10 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as E,b,S as C,c as 
               data-bs-toggle="tooltip"
               data-bs-placement="top"
               data-bs-custom-class="custom-tooltip"
-              data-bs-title="Views: ${S}"
+              data-bs-title="Views: ${T}"
             >
               <svg class="info-icon" aria-hidden="true" width="20" height="20">
-                <use href="/sprite.svg#views"></use>
+                <use href="${u}#views"></use>
               </svg>
             </button>
             <button
@@ -49,10 +49,10 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as E,b,S as C,c as 
               data-bs-toggle="tooltip"
               data-bs-placement="top"
               data-bs-custom-class="custom-tooltip"
-              data-bs-title="Comments: ${T}"
+              data-bs-title="Comments: ${A}"
             >
               <svg class="info-icon" aria-hidden="true" width="20" height="20">
-                <use href="/sprite.svg#comment"></use>
+                <use href="${u}#comment"></use>
               </svg>
             </button>
             <button
@@ -61,11 +61,11 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{T as E,b,S as C,c as 
               data-bs-toggle="tooltip"
               data-bs-placement="top"
               data-bs-custom-class="custom-tooltip"
-              data-bs-title="Downloads: ${A}"
+              data-bs-title="Downloads: ${E}"
             >
               <svg class="info-icon" aria-hidden="true" width="20" height="20">
-                <use href="/sprite.svg#download"></use>
+                <use href="${u}#download"></use>
               </svg>
             </button>
           </div>
-        </a>`},""),$=(t="[data-gallery]")=>{if(!t)return;const{height:e}=document.querySelector("[data-gallery]").firstElementChild.getBoundingClientRect();window.scrollBy({top:e*2,behavior:"smooth"})},y=()=>localStorage.getItem("theme"),B=t=>localStorage.setItem("theme",t),m=()=>{const t=y();return t||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light")},h=t=>{t==="auto"?document.documentElement.setAttribute("data-bs-theme",window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"):document.documentElement.setAttribute("data-bs-theme",t)};h(m());const g=(t,e=!1)=>{const o=document.querySelector("#bd-theme");if(!o)return;const s=document.querySelector("#bd-theme-text"),i=document.querySelector(".theme-icon-active use"),a=document.querySelector(`[data-bs-theme-value="${t}"]`),r=a.querySelector("svg use").getAttribute("href");document.querySelectorAll("[data-bs-theme-value]").forEach(l=>{l.classList.remove("active"),l.setAttribute("aria-pressed","false")}),a.classList.add("active"),a.setAttribute("aria-pressed","true"),i.setAttribute("href",r);const u=`${s.textContent} (${a.dataset.bsThemeValue})`;o.setAttribute("aria-label",u),e&&o.focus()};window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",()=>{const t=y();t!=="light"&&t!=="dark"&&h(m())});window.addEventListener("DOMContentLoaded",()=>{g(m()),document.querySelectorAll("[data-bs-theme-value]").forEach(t=>{t.addEventListener("click",()=>{const e=t.getAttribute("data-bs-theme-value");B(e),h(e),g(e,!0)})})});const n={searchForm:document.forms.searchForm,container:document.querySelector("[data-gallery]"),loadMoreBtn:document.querySelector("[data-load-more]")},c={currentPage:1,searchQuery:""},w=t=>{new C(t).refresh()},L=t=>{[...document.querySelectorAll(t)].map(o=>new x(o))},P=async t=>{t.preventDefault();const e=t.currentTarget;c.searchQuery=e.elements.searchInput.value,c.currentPage=1;try{n.container.innerHTML="",n.loadMoreBtn.classList.add("d-none");const{hits:o,isEndOfCollection:s}=await p(c);if(n.container.insertAdjacentHTML("beforeend",v(o)),w("[data-gallery] a"),L('[data-bs-toggle="tooltip"]'),s)return;n.loadMoreBtn.classList.remove("d-none")}catch(o){console.log(o)}finally{e.reset()}},I=async t=>{try{c.currentPage+=1;const{hits:e,isEndOfCollection:o}=await p(c);n.container.insertAdjacentHTML("beforeend",v(e)),w("[data-gallery] a"),L('[data-bs-toggle="tooltip"]'),$("[data-gallery]"),o&&n.loadMoreBtn.classList.add("d-none")}catch(e){console.log(e)}};n.searchForm.addEventListener("submit",P);n.loadMoreBtn.addEventListener("click",I);
+        </a>`},""),B=(t="[data-gallery]")=>{if(!t)return;const{height:e}=document.querySelector("[data-gallery]").firstElementChild.getBoundingClientRect();window.scrollBy({top:e*2,behavior:"smooth"})},w=()=>localStorage.getItem("theme"),P=t=>localStorage.setItem("theme",t),h=()=>{const t=w();return t||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light")},b=t=>{t==="auto"?document.documentElement.setAttribute("data-bs-theme",window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"):document.documentElement.setAttribute("data-bs-theme",t)};b(h());const g=(t,e=!1)=>{const o=document.querySelector("#bd-theme");if(!o)return;const s=document.querySelector("#bd-theme-text"),c=document.querySelector(".theme-icon-active use"),a=document.querySelector(`[data-bs-theme-value="${t}"]`),n=a.querySelector("svg use").getAttribute("href");document.querySelectorAll("[data-bs-theme-value]").forEach(l=>{l.classList.remove("active"),l.setAttribute("aria-pressed","false")}),a.classList.add("active"),a.setAttribute("aria-pressed","true"),c.setAttribute("href",n);const m=`${s.textContent} (${a.dataset.bsThemeValue})`;o.setAttribute("aria-label",m),e&&o.focus()};window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",()=>{const t=w();t!=="light"&&t!=="dark"&&b(h())});window.addEventListener("DOMContentLoaded",()=>{g(h()),document.querySelectorAll("[data-bs-theme-value]").forEach(t=>{t.addEventListener("click",()=>{const e=t.getAttribute("data-bs-theme-value");P(e),b(e),g(e,!0)})})});const r={searchForm:document.forms.searchForm,container:document.querySelector("[data-gallery]"),loadMoreBtn:document.querySelector("[data-load-more]")},i={currentPage:1,searchQuery:""},L=t=>{new C(t).refresh()},S=t=>{[...document.querySelectorAll(t)].map(o=>new x(o))},I=async t=>{t.preventDefault();const e=t.currentTarget;i.searchQuery=e.elements.searchInput.value,i.currentPage=1;try{r.container.innerHTML="",r.loadMoreBtn.classList.add("d-none");const{hits:o,isEndOfCollection:s}=await v(i);if(r.container.insertAdjacentHTML("beforeend",y(o)),L("[data-gallery] a"),S('[data-bs-toggle="tooltip"]'),s)return;r.loadMoreBtn.classList.remove("d-none")}catch(o){console.log(o)}finally{e.reset()}},H=async t=>{try{i.currentPage+=1;const{hits:e,isEndOfCollection:o}=await v(i);r.container.insertAdjacentHTML("beforeend",y(e)),L("[data-gallery] a"),S('[data-bs-toggle="tooltip"]'),B("[data-gallery]"),o&&r.loadMoreBtn.classList.add("d-none")}catch(e){console.log(e)}};r.searchForm.addEventListener("submit",I);r.loadMoreBtn.addEventListener("click",H);
