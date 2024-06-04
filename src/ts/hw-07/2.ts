07d9411ce59f5db0df9a15cbfd8713dd41f5a8f4
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-06-04 19:39:03 +0300

    finished hw-07

---
+++ src/ts/hw-07/2.ts
@@ -0,0 +1,45 @@
+/*
+  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
+
+  Клас Employee повинен включати:
+
+  властивість name, яка буде доступна всім.
+  властивість department, яка буде доступна лише всередині класу Employee.
+  salary, яке буде доступне лише всередині класу Employee та його підкласів.
+
+
+  Клас Manager повинен бути підклас класу Employee
+
+  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
+
+*/
+
+class Employee {
+  // Заповніть модифікатори доступу
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
+    this.increaseSalary();
+  }
+
+  getEmployeeDetails(): string {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+  increaseSalary(this: Employee): void {
+    this.salary += 10_000;
+  }
+}
+
+class Manager extends Employee {
+  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+  }
+}
+
+const manager1 = new Manager('Mango', 'clouds', 10_000);
+
+console.log('manager1Details:', manager1.getEmployeeDetails());
+export {};
---
+++ src/ts/hw-07/4.ts
@@ -0,0 +1,61 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(this: Key): number {
+    return this.signature;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: number): void;
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): number {
+    return this.key.getSignature();
+  }
+}
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(keySignature: number): void {
+    if (this.key.getSignature() === keySignature) {
+      this.door = true;
+      console.log(`Двері будинку відчинені.`);
+    } else {
+      console.log(`Двері будинку зачинені.`);
+    }
+  }
+}
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
